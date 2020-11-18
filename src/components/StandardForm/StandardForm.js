3fb33b3137b7d67a1dbef3ce06efb24e323a7258
Edyta2801 <edyta.szarowska@interia.pl>
2020-11-18 14:12:52 +0100

    add tags

--- src/components/StandardForm/StandardForm.js
+++ src/components/StandardForm/StandardForm.js
@@ -4,9 +4,35 @@ import React, { Component } from 'react';
 class StandardForm extends Component {
     render() {
         return (
-            <h2>
-                Standard Form
-            </h2>
+            <div>
+                <h5>Standard Form</h5>
+                <form>
+                    {/* Username */}
+                    <div className='form-group'>
+                        <label htmlFor='username'>Username</label>
+                        <input type='text' className='form-control' id='username' />
+                    </div>
+
+                    {/* Email */}
+                    <div className='form-group'>
+                        <label htmlFor='email'>Email</label>
+                        <input type='email' className='form-control' id='email' />
+                    </div>
+
+                    {/* Passworrd */}
+                    <div className='form-group'>
+                        <label htmlFor='password'>Password</label>
+                        <input type='password' className='form-control' id='password' />
+                    </div>
+
+                    {/* Confirm Password */}
+                    <div className='form-group'>
+                        <label htmlFor='confirmPassword'>Password</label>
+                        <input type='password' className='form-control' id='confirmPassword' />
+                    </div>
+
+                </form>
+            </div>
         );
     }
 }
