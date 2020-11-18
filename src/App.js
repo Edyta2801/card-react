109c31ceb542692459dd2097b22e672a6cd3efd2
Edyta2801 <edyta.szarowska@interia.pl>
2020-11-18 14:23:54 +0100

    add state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import {Card} from './components/Card';
-import {StandardForm } from './components/StandardForm';
+import { Card } from './components/Card';
+import { StandardForm } from './components/StandardForm';
 
 
 
@@ -14,7 +14,7 @@ function App() {
        excerpt="This is a short card description."
        showMore="This is a long card description, lorem ipsum dolor sit amet. This is a long card destiption, lorem ipsum dolor sit amet. This is a long card destiption, lorem ipsum dolor sit amet."
       /> */}
-      <StandardForm/>
+      <StandardForm />
     </div>
   );
 }
--- src/components/StandardForm/StandardForm.js
+++ src/components/StandardForm/StandardForm.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 
 
+
 class StandardForm extends Component {
+    state = {
+        username: '', usernameValid: false,
+        email: '', emailValid: false,
+        password: '', passwordValid: false,
+        passwordConfirm: '', passwordConfirmValid: false,
+        formaValid: false,
+        errorMsh: {}
+    }
+
+
     render() {
         return (
             <div>
@@ -10,25 +21,25 @@ class StandardForm extends Component {
                     {/* Username */}
                     <div className='form-group'>
                         <label htmlFor='username'>Username</label>
-                        <input type='text' className='form-control' id='username' />
+                        <input type='text' className='form-control' id='username' value={this.state.username} />
                     </div>
 
                     {/* Email */}
                     <div className='form-group'>
                         <label htmlFor='email'>Email</label>
-                        <input type='email' className='form-control' id='email' />
+                        <input type='email' className='form-control' id='email' value={this.state.email} />
                     </div>
 
                     {/* Passworrd */}
                     <div className='form-group'>
                         <label htmlFor='password'>Password</label>
-                        <input type='password' className='form-control' id='password' />
+                        <input type='password' className='form-control' id='password' value={this.state.password} />
                     </div>
 
                     {/* Confirm Password */}
                     <div className='form-group'>
                         <label htmlFor='confirmPassword'>Password</label>
-                        <input type='password' className='form-control' id='confirmPassword' />
+                        <input type='password' className='form-control' id='confirmPassword' value={this.state.confirmPassword} />
                     </div>
 
                 </form>
