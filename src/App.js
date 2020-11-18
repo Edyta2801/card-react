20ec6f615ea896b585e07fd2b8d755a38f503a72
Edyta2801 <edyta.szarowska@interia.pl>
2020-11-18 13:24:29 +0100

    add StandardForm component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
 import {Card} from './components/Card';
+import {StandardForm } from './components/StandardForm';
 
 
 
 function App() {
   return (
     <div className="App">
-      <Card
+      {/* <Card
        headline="Headline"
        image="https://picsum.photos/300/200"
        excerpt="This is a short card description."
        showMore="This is a long card description, lorem ipsum dolor sit amet. This is a long card destiption, lorem ipsum dolor sit amet. This is a long card destiption, lorem ipsum dolor sit amet."
-      />
+      /> */}
+      <StandardForm/>
     </div>
   );
 }
---
+++ src/components/StandardForm/StandardForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class StandardForm extends Component {
+    render() {
+        return (
+            <h2>
+                Standard Form
+            </h2>
+        );
+    }
+}
+
+export default StandardForm;
