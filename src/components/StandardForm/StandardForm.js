d03800a778f30d2fed8bcce1b0f14f0ca75c6985
Edyta2801 <edyta.szarowska@interia.pl>
2020-11-20 11:14:23 +0100

    add validate for username input

--- src/components/StandardForm/StandardForm.js
+++ src/components/StandardForm/StandardForm.js
@@ -12,6 +12,29 @@ class StandardForm extends Component {
         errorMsh: {}
     }
 
+    handleChange = (event) => {
+        const { target: { id, value } } = event
+        this.setState({ [id]: value })
+    };
+
+    
+    
+    validateUsername = () => {
+        const { username } = this.state;
+        let usernameValid = true;
+        let errorMsg = { ...this.state.errorMsg }
+
+        if (username.length < 6 || username.length > 15) {
+            usernameValid = false;
+            errorMsg.username = 'Username should be betweeen 6 and 15 characters'
+        }
+        this.setState({ usernameValid, errorMsg });
+    }
+
+    
+
+
+
 
     render() {
         return (
@@ -21,25 +44,26 @@ class StandardForm extends Component {
                     {/* Username */}
                     <div className='form-group'>
                         <label htmlFor='username'>Username</label>
-                        <input type='text' className='form-control' id='username' value={this.state.username} />
+                        <input type='text' className='form-control' id='username' value={this.state.username}
+                            onChange={this.handleChange} />
                     </div>
 
                     {/* Email */}
                     <div className='form-group'>
                         <label htmlFor='email'>Email</label>
-                        <input type='email' className='form-control' id='email' value={this.state.email} />
+                        <input type='email' className='form-control' id='email' value={this.state.email} onChange={this.handleChange} />
                     </div>
 
                     {/* Passworrd */}
                     <div className='form-group'>
                         <label htmlFor='password'>Password</label>
-                        <input type='password' className='form-control' id='password' value={this.state.password} />
+                        <input type='password' className='form-control' id='password' value={this.state.password} onChange={this.handleChange} />
                     </div>
 
                     {/* Confirm Password */}
                     <div className='form-group'>
-                        <label htmlFor='confirmPassword'>Password</label>
-                        <input type='password' className='form-control' id='confirmPassword' value={this.state.confirmPassword} />
+                        <label htmlFor='confirmPassword'>Confirm Password </label>
+                        <input type='password' className='form-control' id='confirmPassword' value={this.state.confirmPassword} onChange={this.handleChange} />
                     </div>
 
                 </form>
